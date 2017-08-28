8daa86221434350113fe5f6f57981b58635a8d87
Bra1nwash3d <Bra1nwash3d@users.noreply.github.com>
2017-08-28 22:47:13 +0200

    Fixing replay compatibility

--- Mod TemplateGroups/template_groups/hook/lua/ui/game/gamemain.lua
+++ Mod TemplateGroups/template_groups/hook/lua/ui/game/gamemain.lua
@@ -4,15 +4,17 @@ local originalCreateUI_templategroups = CreateUI
 function CreateUI(isReplay)
 	originalCreateUI_templategroups(isReplay)
 	local KeyMapper = import('/lua/keymap/keymapper.lua')
-	for i = 1, 5 do
-		local s = ''..i
-		KeyMapper.SetUserKeyAction(
-			"Templates beginning with "..s,
-			{
-				action = "UI_Lua import('/lua/keymap/hotbuild.lua').buildActionTemplate(0, "..s..")",
-				category = "Template Groups",
-				order = i,
-			}
-		)
+	if not isReplay then
+		for i = 1, 5 do
+			local s = ''..i
+			KeyMapper.SetUserKeyAction(
+				"Templates beginning with "..s,
+				{
+					action = "UI_Lua import('/lua/keymap/hotbuild.lua').buildActionTemplate(0, "..s..")",
+					category = "Template Groups",
+					order = i,
+				}
+			)
+		end
 	end
 end
